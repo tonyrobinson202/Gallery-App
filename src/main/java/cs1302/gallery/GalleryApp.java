3bea72e029cc32fcbf03d07e51b0507e733ea0b9
PAUL ROBINSON <paul@nike.cs.uga.edu>
2018-11-07 17:18:12 -0500

    commit 2

--- src/main/java/cs1302/gallery/GalleryApp.java
+++ src/main/java/cs1302/gallery/GalleryApp.java
@@ -1,18 +1,164 @@
-
 package cs1302.gallery;
+/**                                                                            
+ *@author Tony Robinson                                                        
+ *Gallery App extends Application                                                   
+ *Provides a search query that uploads images relevant to what the user enters                    
+ *cs1302                                                                       
+ */
 
+import javafx.scene.control.ProgressBar;
+import java.util.Random;
+import javafx.stage.Modality;
+import javafx.animation.Timeline;
+import javafx.animation.KeyFrame;
+import java.time.LocalTime;
+import javafx.util.Duration;
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.event.EventHandler;
+import javafx.event.ActionEvent;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToolBar;
+import javafx.scene.text.Text;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyCodeCombination;
+import javafx.scene.input.KeyCombination;
+import javafx.scene.control.Separator;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.ImageViewBuilder;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonArray;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.io.IOException;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class GalleryApp extends Application {
+    String encodedString = null;
+    URL url = null;
+    InputStreamReader reader = null;
+    String word = "rock";
+    GridPane gridPane = new GridPane();
+    int counter = 0;
+    Timeline timeline = new Timeline();
+    ProgressBar progress = new ProgressBar();
 
     @Override
     public void start(Stage stage) {
-	HBox pane = new HBox();
-        Scene scene = new Scene(pane);
+	VBox vbox= new VBox();
+
+	BorderPane borderPane = new BorderPane();
+	HBox hbox = new HBox();
+	
+	final Menu file = new Menu("File");
+	final Menu help = new Menu("Help");
+ 
+	MenuBar menuBar = new MenuBar();
+	menuBar.getMenus().addAll(file, help);
+
+	MenuItem exitItem = new MenuItem("Exit");
+	MenuItem aboutMe = new MenuItem("About");
+	file.getItems().add(exitItem);
+	help.getItems().add(aboutMe);
+
+	aboutMe.setOnAction(e ->{
+		FlowPane flowPane3 = new FlowPane();
+		Text info  = new Text("About Name: Tony Robinson\n Email: par60406@uga.edu\n Version: 1.02 Alpha \nImage: ");
+		Image me = new Image("https://scontent-atl3-1.xx.fbcdn.net/v/t1.0-9/22885832_105718290197745_5007537168561701317_n.jpg?oh=7cd349cc08b0b70384668e367c227405&oe=5AA77A66");
+		flowPane3.getChildren().add(info);
+		flowPane3.getChildren().add(new ImageView(me));
+
+		Stage stage3 = new Stage();
+		Scene scene3 = new Scene(flowPane3);
+		stage3.setScene(scene3);
+		stage3.initModality(Modality.APPLICATION_MODAL);
+		stage3.setTitle("Aboout Me");
+		stage3.sizeToScene();
+		stage3.showAndWait();
+	    });//creates the about me pop up window
+
+        aboutMe.setAccelerator(new KeyCodeCombination(KeyCode.A, KeyCombination.CONTROL_DOWN));//allows hot keys for the help option
+	
+        exitItem.setOnAction(e ->{
+		Platform.exit();
+		System.exit(0);
+	    });
+	exitItem.setAccelerator(new KeyCodeCombination(KeyCode.E, KeyCombination.CONTROL_DOWN));//allows hot keys for the exit option
+
+	ToolBar toolbar = new ToolBar();
+
+	Button pause = new Button("Pause");
+	Text query = new Text("Search Query");
+	TextField search = new TextField();
+	Button update = new Button("Update Images");
+	
+	toolbar.getItems().add(pause);
+	toolbar.getItems().add(query);
+	toolbar.getItems().add(search);
+	toolbar.getItems().add(update);
+	//creates the toolbar
+
+	vbox.getChildren().add(menuBar);
+	vbox.getChildren().add(toolbar);
+	
+	borderPane.setTop(vbox);
+	borderPane.setBottom(hbox);
+	borderPane.setCenter(gridPane);
+	borderPane.setBottom(progress);
+	//creates the main pane where everything is placed
+
+	EventHandler<ActionEvent> handler = event -> Play();
+	KeyFrame keyFrame = new KeyFrame(Duration.seconds(2), handler);
+	timeline.setCycleCount(Timeline.INDEFINITE);
+	timeline.getKeyFrames().add(keyFrame);
+	timeline.play();
+	//decides how often the images will be swapped
+
+	pause.setOnAction(new EventHandler<ActionEvent>(){
+		public void handle(ActionEvent event){
+		    if(counter == 1)
+			{
+			    timeline.pause();
+			}
+		    
+		    if(pause.getText().equals("Play"))
+		       {
+			   pause.setText("Pause");
+			   timeline.play();
+		       }//sets play to pause and has images swicht out
+		       
+		    else  if(pause.getText().equals("Pause"))
+			{
+			    pause.setText("Play");
+			    timeline.pause();
+			}//sets pause to play and stops images from moving
+		    }
+	    });
+
+	update.setOnAction(new EventHandler<ActionEvent>(){
+                public void handle(ActionEvent event){
+                     word = search.getText();
+		     Update(word);
+		    }	
+	    });//adds the word in the search query to the url
+	Update(word);
+
+	Scene scene = new Scene(borderPane);       	
 	stage.setMaxWidth(640);
 	stage.setMaxHeight(480);
         stage.setTitle("[XYZ] Gallery!");
@@ -32,6 +178,112 @@ public class GalleryApp extends Application {
 	    System.exit(1);
 	} // try
     } // main
+    /**
+     *swaps out all the current images with images relevant to the text entered in the search query
+     *@param input the word entered into the search query
+     *
+     */
+    public  void Update(String input){
+	try{
+            encodedString = URLEncoder.encode(input,"UTF-8");
+        }catch(java.io.UnsupportedEncodingException e){
+            // stuff                                                          
+        } // try                                                                                   
+        if(encodedString != null){
+            try{
+                url = new URL("http://itunes.apple.com/search?term=" + encodedString);
+            }catch(java.net.MalformedURLException e){
+                // stuff                                                       
+            } // try                                                           
+        } // if                                                                                     
+        try{
+            reader = new InputStreamReader(url.openStream());
+        }catch(IOException e){
+        }//try                                                                                      
+        JsonParser jp = new JsonParser();
+        JsonElement je = jp.parse(reader);
 
+	JsonObject root = je.getAsJsonObject(); 
+        JsonArray results = root.getAsJsonArray("results"); // "results" array      
+        int numResults = results.size();// "results" array size     
+	
+	if(numResults >= 20){
+	    counter = 0;
+	    int x = 0, y =0;
+
+	    for (int i = 0; i < 20; i++) {
+		JsonObject result = results.get(i).getAsJsonObject();
+		JsonElement artworkUrl100 = result.get("artworkUrl100");
+		if (artworkUrl100 != null) {
+		    String artUrl = artworkUrl100.getAsString();
+		    Image image = new Image(artUrl, 100, 100, false, false);
+		    gridPane.add(new ImageView(image),y,x );
+		    y++;
+		    if(y == 5){
+			y = 0;
+			x++;
+			progress.setProgress(.05 * i);
+		    }
+		} // if                                                       
+	    } // for
+	}//uses the url to grab the image abojects and store them in the grid pane
+
+	else{
+	    timeline.pause();
+	    FlowPane flowPane2 = new FlowPane();
+	    Text info  = new Text("improper search, exit and try again");
+	    flowPane2.getChildren().add(info);
+
+	    Stage stage2 = new Stage();
+	    Scene scene2 = new Scene(flowPane2, 300, 100);
+	    stage2.setScene(scene2);
+	    stage2.initModality(Modality.APPLICATION_MODAL);
+	    stage2.setTitle("Ooops");
+	    stage2.sizeToScene();
+	    stage2.showAndWait();	
+	}//creates a pop up window when the search has less than 20 results
+
+    }//updates the images based on what's put in the search query 
+    /**
+     *swaps out the imgages with another image in sthe search query every 2 seconds
+     */
+    public void Play(){
+	Random rand = new Random();
+	
+	try{
+            encodedString = URLEncoder.encode(word,"UTF-8");
+        }catch(java.io.UnsupportedEncodingException e){
+        } // try                                                               
+        if(encodedString != null){
+            try{
+                url = new URL("http://itunes.apple.com/search?term=" + encodedString);
+            }catch(java.net.MalformedURLException e){
+                // stuff                                                       
+            } // try                                                           
+        } // if
+	
+	try{
+            reader = new InputStreamReader(url.openStream());
+        }catch(IOException e){
+        }//try                                                                 
+
+        JsonParser jp = new JsonParser();
+        JsonElement je = jp.parse(reader);
+
+        JsonObject root = je.getAsJsonObject();
+        JsonArray results = root.getAsJsonArray("results");
+	
+	int y = rand.nextInt(4) + 0;
+        int x = rand.nextInt(3) + 0;
+        int imageNum = 21 + rand.nextInt((results.size()-22) + 1);
+	
+	JsonObject result = results.get(imageNum).getAsJsonObject();
+	JsonElement artworkUrl100 = result.get("artworkUrl100");
+	
+	String artUrl = artworkUrl100.getAsString();
+	Image image = new Image(artUrl, 100, 100, false, false);
+	gridPane.add(new ImageView(image),y,x );
+    }//executes the picture swap every two seconds
+    
 } // GalleryApp
 
